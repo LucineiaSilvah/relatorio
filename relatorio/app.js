b1cbcff3f509a8a044d8578e7014e10fc1a24bfe
LucineiaSilvah <lucineia.rsilvah@gmail.com>
2025-01-10 01:11:15 -0300

    add atualizacoes

---
+++ relatorio/app.js
@@ -0,0 +1,87 @@
+let form = document.getElementById('form');
+let alimentacao =  document.querySelector('.alimentacao');
+let fralda =  document.querySelector('.fralda');
+let banho =  document.querySelector('.banho');
+let descanso =  document.querySelector('.descanso');
+const btnAdd = document.querySelector('.btnAdd');
+const btnfralda = document.querySelector('.btnfralda');
+const btnbanho = document.querySelector('.btnbanho');
+const btndescanso = document.querySelector('.btndescanso');
+
+
+btnAdd.addEventListener('click', (e)=>{
+  e.preventDefault()
+  const novoEl = document.createElement('input')
+  const br = document.createElement('br')
+  novoEl.type = 'time'
+
+
+ 
+  alimentacao.appendChild(novoEl)
+  alimentacao.appendChild(br)
+})
+
+btnfralda.addEventListener('click', (e)=>{
+  e.preventDefault()
+  const novoEl = document.createElement('input')
+  const br = document.createElement('br')
+  novoEl.type = 'time'
+
+
+ 
+  fralda.appendChild(novoEl)
+  fralda.appendChild(br)
+})
+
+
+btnbanho.addEventListener('click', (e)=>{
+  e.preventDefault()
+  const novoEl = document.createElement('input')
+  const br = document.createElement('br')
+  novoEl.type = 'time'
+
+
+ 
+  banho.appendChild(novoEl)
+  banho.appendChild(br)
+})
+
+btndescanso.addEventListener('click', (e)=>{
+  e.preventDefault()
+  const novoEl = document.createElement('input')
+  const novo = document.createElement('input')
+  const p = document.createElement('p')
+  const conteudo = document.createTextNode('à')
+  const br = document.createElement('br')
+  novoEl.type = 'time'
+  novo.type = 'time'
+p.appendChild(conteudo)
+
+ 
+  descanso.appendChild(novoEl) 
+  descanso.appendChild(conteudo)
+  descanso.appendChild(novo)
+  descanso.appendChild(br)
+
+ 
+
+
+})
+
+ document.getElementById('screenshotBtn').addEventListener('click', () => {
+      const element = document.getElementById('capture');
+      html2canvas(element, {
+        useCORS: true, // Garante que imagens de fundo externas sejam carregadas
+        allowTaint: false, // Impede a alteração do canvas por políticas de segurança
+        scale: 2 // Aumenta a qualidade da captura
+      }).then(canvas => {
+        // Adiciona o canvas à página (apenas para visualizar)
+        document.body.appendChild(canvas);
+
+        // Baixa a imagem
+        const link = document.createElement('a');
+        link.download = 'screenshot.png';
+        link.href = canvas.toDataURL('image/png');
+        link.click();
+      });
+    });
